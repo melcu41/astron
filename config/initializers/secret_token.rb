08989caf7cd6b6b92f9550256e1cfdc32edb30b4
Melanio Cu <melcu41@yahoo.com>
2012-06-26 18:13:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astron::Application.config.secret_token = '8186ba8ba29e45bc17bf3154c9a34b6105093680ce5f1f440b2d8acf1942beabe664c9b48f0a7057dfd1b346fb4ddeb9d3b80d6a47200e20c0d904c76418e470'
