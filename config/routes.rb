ed1f611ce638fad77f61976214a6f067d3384720
Melanio Cu <melcu41@yahoo.com>
2012-06-26 21:15:49 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Astron::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"
+
+  root to: 'static_pages#home'
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about"
+  #get "static_pages/contact"
+  match '/signup',  to: 'users#new'
+  
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +61,7 @@ Astron::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root to: 'static_pages#home'
+   
 
   # See how all your routes lay out with "rake routes"
 
