24ca476653fbe5577327a10c19fc2e63fece4a78
Melanio Cu <melcu41@yahoo.com>
2012-06-27 13:55:29 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Astron::Application.routes.draw do
   #get "users/new"
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   root to: 'static_pages#home'
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
